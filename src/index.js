505637d7616e462da5115b081cbad40a3d821019
Sacha Morgese <sachamorgese@gmail.com>
2017-11-09 23:23:06 +0900

    Updated index.js.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
