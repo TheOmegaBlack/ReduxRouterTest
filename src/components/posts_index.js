9cc77d7db5a4d72521bb3b204503d06bb14f1789
Sacha Morgese <sacha_morgese@waku-2.com>
2017-11-10 18:27:40 +0900

    Added several components and reducers

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import _ from "lodash"
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchPosts } from "../actions/index";
+
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+  
+  renderPosts() {
+    console.log(this.props)
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+  
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
-export default PostsIndex
+function mapStateToProps(state) {
+  return { posts: state.posts }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch)
+}
+
+export default connect (mapStateToProps, mapDispatchToProps)(PostsIndex)
